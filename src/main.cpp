c7ef84cbdd1ad486b71d07900a12b6a72dfe0d34
wfox5812 <willffox9183@gmail.com>
2024-04-18 17:05:11 -0400

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -534,7 +534,7 @@ void welcomeScreen() {
   tft.setCursor(xRecovery, start + (offset*3));
   tft.println(F("Recovery"));
 
-  delay(4000);
+  delay(2000);
 }
 
 void promptStart() {
@@ -991,6 +991,7 @@ void stateMachine() {
       if (!poweredOff) {
         POWEROFF(); // UPDATE TFT SCREEN
         poweredOff = true;
+        promptRefreshed = false;
       }
       
  
