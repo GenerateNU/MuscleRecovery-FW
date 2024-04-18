28c4e6e0ed9f8243612ff4a74840018844039276
wfox5812 <willffox9183@gmail.com>
2024-04-17 20:26:27 -0400

    Added EMG files

---
+++ src/EMG.h
@@ -0,0 +1,32 @@
+#ifndef EMG_H
+#define EMG_H
+
+// Include any necessary headers here
+#include "ADS1X15.h"
+#include <cstdint>
+
+// Declare any global constants or macros here
+#define DC_OFFSET_SIZE 1.5
+
+// Declare any global variables here
+float value = 0;
+float prev  = 0;
+uint32_t lastSample = 0;
+uint32_t lastSample2 = 0;
+uint32_t lastTime = 0;
+
+// Declare any class declarations here
+class LowPassFilter {
+private:
+    double alpha;
+    double lastOutput;
+public:
+    LowPassFilter(double cutoffFreq, double samplingRate)
+    double filter(double inputValue)
+};
+
+// Declare any function prototypes here
+void dataAcquisition();
+float ADStomV(uint16_t newValue);
+
+#endif // EMG_H
